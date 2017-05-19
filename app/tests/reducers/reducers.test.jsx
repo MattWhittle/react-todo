8f73b5c387077d786e0b2b724f344967bfd54501
MattWhittle <themwproject@yahoo.com>
2017-05-19 11:51:05 -0400

    add todos reduxer and tests

--- app/tests/reducers/reducers.test.jsx
+++ app/tests/reducers/reducers.test.jsx
@@ -27,4 +27,37 @@ describe('Reducers', ()=>{
             expect(res).toEqual(true);
         });
     });
+    describe('todosReducer', () => {
+        it('should add new todo', ()=>{
+            var action = {
+                type: 'ADD_TODO',
+                text: 'walk the dog'
+            };
+            var res = reducers.todosReducer(df([]), df(action));
+            expect(res.length).toEqual(1);
+            expect(res[0].text).toEqual(action.text);
+
+        });
+
+        // defined todos array with realistic todo item
+        // generate action
+        // call reducer and assert completed flipped
+        it('should toggle todo', ()=>{
+            var todos =  [{
+                id: 11,
+                text: 'Test features',
+                completed: true,
+                createdAt: 123,
+                completedAt: 125
+            }];
+            var action = {
+                type: 'TOGGLE_TODO',
+                id: 11
+            }
+
+            var res = reducers.todosReducer(df(todos), df(action));
+            expect(res[0].completed).toEqual(false);
+            expect(res[0].completedAt).toEqual(undefined);
+        });
+    });
 });
